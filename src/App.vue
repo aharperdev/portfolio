38822c9effa49463c0b225fa987923695920b55b
Sebastian Gaete <sebastian.g@closelly.com>
2024-09-25 12:22:10 -0300

    updated profile picture style

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Resume from './components/Resume.vue'
 <template>
   <main class="container-fluid">
     <div class="row">
-      <div class="col-md-3 col-lg-3 info text-center p-3">
+      <div class="col-md-3 col-lg-3 p-0">
         <Resume></Resume>
       </div>
       <div class="col-md-9 col-lg-9 content p-3">
--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -5,39 +5,43 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 </script>
 
 <template>
-  <div class="d-flex flex-column flex-center pt-3 pt-md-5">
+  <div class="d-flex flex-column flex-center info text-center h-100 w-100">
     <div class="d-flex flex-column align-items-center">
       <img
         class="profile-pic mb-3"
         src="https://avatars.githubusercontent.com/u/24919733"
         alt="avatar"
       />
-      <h4>Sebastian Gaete</h4>
+      <h4>Andre Gaete Barra</h4>
       <h6>Fullstack Developer</h6>
     </div>
-    <hr />
-    <div class="text-start">
-      <h5>Habilidades en:</h5>
-      <ul>
-        <li>Angular/Vue/React</li>
-        <li>Typescript</li>
-        <li>Node</li>
-        <li>Testing Automatizado</li>
-        <li>Ingles Avanzado C1</li>
-        <li>Linux</li>
-      </ul>
-    </div>
-    <hr />
-    <div class="d-flex justify-content-around">
-      <a href="https://github.com/Locksdn">
-        <FontAwesomeIcon :icon="faGithub" size="2x"></FontAwesomeIcon>
-      </a>
-      <a href="https://www.linkedin.com/in/sebastian-gaeteb/">
-        <FontAwesomeIcon :icon="faLinkedin" size="2x"></FontAwesomeIcon>
-      </a>
-      <a href="mailto:dandrebarra@gmail.com">
-        <FontAwesomeIcon :icon="faEnvelope" size="2x"></FontAwesomeIcon>
-      </a>
+    <div class="px-3 pb-5 flex-grow-1 d-flex flex-column justify-content-between">
+      <div class="text-start">
+        <hr />
+        <h5>Habilidades en:</h5>
+        <ul class="list-unstyled ps-1">
+          <li>Angular/Vue/React</li>
+          <li>Typescript</li>
+          <li>Node</li>
+          <li>Testing Automatizado</li>
+          <li>Ingles Avanzado C1</li>
+          <li>Linux</li>
+        </ul>
+      </div>
+      <div>
+        <hr />
+        <div class="d-flex justify-content-around">
+          <a href="https://github.com/Locksdn">
+            <FontAwesomeIcon :icon="faGithub" size="2x"></FontAwesomeIcon>
+          </a>
+          <a href="https://www.linkedin.com/in/sebastian-gaeteb/">
+            <FontAwesomeIcon :icon="faLinkedin" size="2x"></FontAwesomeIcon>
+          </a>
+          <a href="mailto:dandrebarra@gmail.com">
+            <FontAwesomeIcon :icon="faEnvelope" size="2x"></FontAwesomeIcon>
+          </a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
